2eeddfaa14b873d7a16e6a97d048320e3baa4acc
JosephLaMantia <joeylamaxd@gmail.com>
2022-04-21 20:57:25 -0400

    Login page functionality complete

--- app/src/main/java/com/group4/habitpal/fragments/LoginFragment.kt
+++ app/src/main/java/com/group4/habitpal/fragments/LoginFragment.kt
@@ -1,13 +1,17 @@
 package com.group4.habitpal.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.group4.habitpal.R
 import com.group4.habitpal.activities.TitleActivity
 import com.group4.habitpal.custom_views.CustomAppButton
+import com.parse.ParseUser
 
 
 class LoginFragment : Fragment() {
@@ -27,9 +31,32 @@ class LoginFragment : Fragment() {
         val titleActivity = requireActivity() as TitleActivity
         val btnLogin = titleActivity.findViewById<CustomAppButton>(R.id.loginfrag_btn_login)
 
-        btnLogin.setAction {
-            titleActivity.goToHomeScreen()
-        }
 
+        btnLogin.setOnClickListener {
+            val username = view.findViewById<EditText>(R.id.field_email).text.toString()
+            val password = view.findViewById<EditText>(R.id.field_password).text.toString()
+            loginUser(username, password)
+        }
+    }
+
+    private fun loginUser(username: String, password: String) {
+        ParseUser.logInInBackground(username, password, ({ user, e ->
+            if (user != null) {
+                // Hooray!  The user is logged in.
+                Toast.makeText(requireContext(), "Successfully logged in!", Toast.LENGTH_SHORT).show()
+                Log.i(TAG, "Successfully logged in user!")
+                val titleActivity = requireActivity() as TitleActivity
+                titleActivity.goToHomeScreen()
+
+            } else {
+                // login failed.  Look at the ParseException to see what happened.
+                e.printStackTrace()
+                Toast.makeText(requireContext(), "Error logging in", Toast.LENGTH_SHORT).show()
+            }})
+        )
+    }
+
+    companion object{
+        const val TAG = "LoginFragment"
     }
 }
