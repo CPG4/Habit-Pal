f10e2f472f85af82e820f486ebfc927ca628d783
JosephLaMantia <joeylamaxd@gmail.com>
2022-04-27 19:51:25 -0400

    Created habit class & implemented add habit functionality

--- app/src/main/java/com/group4/habitpal/Habit.kt
+++ app/src/main/java/com/group4/habitpal/Habit.kt
@@ -23,6 +23,52 @@ package com.group4.habitpal
  * year summary
  *
  */
-class Habit {
-    // TODO: fields, constructors, functions, javadoc
+
+import com.parse.ParseClassName
+import com.parse.ParseFile
+import com.parse.ParseObject
+import com.parse.ParseUser
+
+@ParseClassName("Habit")
+class Habit : ParseObject() {
+
+    fun getName(): String? {
+        return getString(KEY_NAME)
+    }
+
+    fun setName(name: String) {
+        put(KEY_NAME, name)
+    }
+
+    fun getType(): String? {
+        return getString(KEY_TYPE)
+    }
+
+    fun setType(type: String) {
+        put(KEY_TYPE, type)
+    }
+
+    fun getGoal(): Number? {
+        return getNumber(KEY_GOAL)
+    }
+
+    fun setGoal(goal: Number) {
+        put(KEY_GOAL, goal)
+    }
+
+    fun getUser(): ParseUser? {
+        return getParseUser(KEY_USER)
+    }
+
+    fun setUser(user: ParseUser) {
+        put(KEY_USER, user)
+    }
+
+    companion object{
+        const val KEY_NAME = "name"
+        const val KEY_TYPE = "type"
+        const val KEY_GOAL = "goal"
+        const val KEY_USER = "user"
+    }
+
 }
