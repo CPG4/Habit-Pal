eb7fe0831d115c8e5f18fb8bcbbc51a097e7322b
JosephLaMantia <joeylamaxd@gmail.com>
2022-05-02 17:36:06 -0400

    Implemented recycler view for my habits screen.

---
+++ app/src/main/java/com/group4/habitpal/HabitAdapter.kt
@@ -0,0 +1,43 @@
+package com.group4.habitpal
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class HabitAdapter(val context: Context, val habits:List<Habit>): RecyclerView.Adapter<HabitAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HabitAdapter.ViewHolder {
+        //specify the layout file to use for this item
+        val view = LayoutInflater.from(context).inflate(R.layout.item_habit, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: HabitAdapter.ViewHolder, position: Int) {
+        val habit = habits[position]
+        holder.bind(habit)
+    }
+
+    override fun getItemCount(): Int {
+        return habits.size
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tvType: TextView
+        val tvDescription: TextView
+        val tvProgress: TextView
+
+        init {
+            tvType = itemView.findViewById(R.id.tvType)
+            tvDescription = itemView.findViewById(R.id.tvDescription)
+            tvProgress = itemView.findViewById(R.id.tvProgress)
+        }
+
+        fun bind(habit: Habit) {
+            tvType.text = habit.getType()
+            tvDescription.text = habit.getName()
+            tvProgress.text = habit.getGoal().toString()
+        }
+    }
+}
--- app/src/main/java/com/group4/habitpal/fragments/MyHabitsFragment.kt
+++ app/src/main/java/com/group4/habitpal/fragments/MyHabitsFragment.kt
@@ -11,14 +11,25 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.view.children
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.group4.habitpal.Habit
+import com.group4.habitpal.HabitAdapter
 import com.group4.habitpal.R
 import com.group4.habitpal.activities.MainActivity
+import com.parse.FindCallback
+import com.parse.ParseException
 import com.parse.ParseQuery
 import com.parse.ParseUser
 
 
 class MyHabitsFragment : Fragment() {
+
+    lateinit var habitsRecyclerView: RecyclerView
+
+    lateinit var adapter: HabitAdapter
+
+    var allHabits: MutableList<Habit> = mutableListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,16 +44,58 @@ class MyHabitsFragment : Fragment() {
 
         val mainActivity = requireActivity() as MainActivity
 
-        val habitListIterator = mainActivity
-            .findViewById<LinearLayout>(R.id.habitlist)
-            .children
-            .iterator()
+//        val habitListIterator = mainActivity
+//            .findViewById<LinearLayout>(R.id.habitlist)
+//            .children
+//            .iterator()
+//
+//        habitListIterator.forEach { v ->
+//            v.setOnClickListener {
+//                mainActivity.replaceFragment(HabitDetailFragment(null))
+//            }
+//        }
 
-        habitListIterator.forEach { v ->
-            v.setOnClickListener {
-                mainActivity.replaceFragment(HabitDetailFragment(null))
+        //Recyclerview implementation
+        habitsRecyclerView = view.findViewById(R.id.habitRecyclerView)
+
+        adapter = HabitAdapter(requireContext(), allHabits)
+        habitsRecyclerView.adapter = adapter
+        habitsRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        queryHabits()
+    }
+
+    fun queryHabits() {
+        // Define the class we would like to query
+        val query: ParseQuery<Habit> = ParseQuery.getQuery(Habit::class.java)
+        // Define our query conditions
+        query.whereEqualTo("user", ParseUser.getCurrentUser())
+        // Execute the find asynchronously
+        query.findInBackground (object : FindCallback<Habit> {
+            override fun done(habits: MutableList<Habit>?, e: ParseException?) {
+                if (e != null) {
+                    //something has gone wrong
+                    Log.e(TAG, "Error fetching habits")
+
+                } else {
+                    if (habits != null) {
+                        for (habit in habits) {
+                            Log.i(
+                                TAG,
+                                "Habit: " + habit.getName())
+                        }
+
+                        allHabits.addAll(habits)
+                        adapter.notifyDataSetChanged()
+                        Log.i(
+                            TAG,
+                            "Habit: " + adapter.itemCount)
+
+                    }
+                }
             }
         }
+        )
     }
 
     companion object {
